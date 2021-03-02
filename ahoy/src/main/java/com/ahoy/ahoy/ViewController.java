2cbc4be5293a796c32ae9007cb74116d05b4a043
Arvin <aikarvin99@gmail.com>
2021-03-02 18:01:31 +0800

    Fixed problem with ViewController

--- ahoy/src/main/java/com/ahoy/ahoy/ViewController.java
+++ ahoy/src/main/java/com/ahoy/ahoy/ViewController.java
@@ -1,11 +1,13 @@
 package com.ahoy.ahoy;
 
 import com.ahoy.ahoy.vessel.Vessel;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+@Controller
 public class ViewController {
 
     @GetMapping("/index")
