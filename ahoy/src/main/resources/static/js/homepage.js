b40ca63e54e4b1caab6df3e6e5f815b5d1ebab3f
juzli_ <lixiang.lxw@gmail.com>
2021-04-06 11:44:33 +0800

    Schedule.html getting data from js

--- ahoy/src/main/resources/static/js/homepage.js
+++ ahoy/src/main/resources/static/js/homepage.js
@@ -9,16 +9,12 @@ $(document).ready( function () {
              console.log(response);
              console.log(val);
              console.log(val.voyagePK);
-//               var card = '<div class="flex"><button class="" value = "' + val + '" type="button">' + val.voyage.invoyn + '</button></div>';
-//               let row = "<tr> <td>"+val.voyagePK.abbrvslm+"</td> <td>"val.voyagePK.invoyn+"</td> <td>"+val.status+"</td> <td>"+val.berth"</td><td>"+val.btrdt.split(" ")[1]+"</td></tr>";
                let row = "";
                if (val.berth==null){
                row = "<tr> <td>"+val.voyagePK.abbrvslm+"</td> <td>"+val.voyagePK.invoyn+"</td><td>"+val.status+"</td> <td>"+val.berth+"</td><td>"+val.btrdt.split(" ")[1]+"</td></tr>";
                }else{
                 row = "<tr> <td>"+val.voyagePK.abbrvslm+"</td> <td>"+val.voyagePK.invoyn+"</td><td>"+val.status+"</td> <td>"+val.berth.berthnum+"</td><td>"+val.btrdt.split(" ")[1]+"</td></tr>";
                }
-//                let row = "<tr> <td>"+val.voyagePK.abbrvslm+"</td> <td>"+val.voyagePK.invoyn+"</td><td>"+val.status+"</td> <td>"+val.berth+"</td><td>"+val.btrdt.split(" ")[1]+"</td></tr>"
-
                $(row).insertAfter("#todayVoyageRecord");
              })
            }
