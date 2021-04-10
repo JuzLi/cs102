593c7e0ef45996f51af718a318b7e1dc7035430d
Eaint <eaintthirikhit@gmail.com>
2021-04-10 21:05:41 +0800

    Style suscribe page, and change the Schedule page dropdown

--- ahoy/src/main/resources/static/js/homepagetest.js
+++ ahoy/src/main/resources/static/js/homepagetest.js
@@ -7,26 +7,35 @@ $(document).ready( function () {
              $(".ajax").remove();
 
              $.each(response, function(key,val){
-//             console.log(response);
-//             console.log(val);
-//             console.log(val.voyagePK);
-//               var card = '<div class="flex"><button class="" value = "' + val + '" type="button">' + val.voyage.invoyn + '</button></div>';
-//               let row = "<tr> <td>"+val.voyagePK.abbrvslm+"</td> <td>"val.voyagePK.invoyn+"</td> <td>"+val.status+"</td> <td>"+val.berth"</td><td>"+val.btrdt.split(" ")[1]+"</td></tr>";
+              // console.log("hello");
                let row = "";
                if (val.berth==null){
                row = '<tr class = "ajax"> <td>'+val.voyagePK.abbrvslm+"</td> <td>"+val.voyagePK.invoyn+"</td><td>"+val.status+"</td> <td>"+val.berth+"</td><td>"+val.btrdt.split(" ")[1]+"</td></tr>";
-
               }else{
                 row = '<tr class = "ajax"> <td>'+val.voyagePK.abbrvslm+"</td> <td>"+val.voyagePK.invoyn+"</td><td>"+val.status+"</td> <td>"+ (val.berth.berthnum) +"</td><td>"+val.btrdt.split(" ")[1]+"</td></tr>";
-
-
               }
-//                let row = "<tr> <td>"+val.voyagePK.abbrvslm+"</td> <td>"+val.voyagePK.invoyn+"</td><td>"+val.status+"</td> <td>"+val.berth+"</td><td>"+val.btrdt.split(" ")[1]+"</td></tr>"
                $(row).insertAfter("#todayVoyageRecord");
              })
            }
          })
-  });
+
+  $.ajax({
+    url: "/ajax/retrieveSubscribedAlerts",
+    type:"GET",
+    dataType:"json",
+    success: function(response){
+      // console.log("hi");
+      $(".alert").remove();
+      // console.log("hi");
+      $.each(response, function(key,val){
+        console.log("hi");
+        var alertrow = '<tr class = "alert"> <td>'+val.alertPK.alerttype +'</td> <td>'+val.alertPK.voyagePK.abbrvslm + '</td><td>' + val.voyage.status + '</td> <td>' + val.voyage.fullinvoyn + '</td><td>' + val.voyage.btrdt.split(" ")[1] +'</td><td>'+val.alertcontent+ '</td></tr>';
+        // var alertrow = '<tr>test</tr>';
+        $(alertrow).insertAfter("#todayAlertRecord")
+      })
+    }
+  })
+});
 
 
 
@@ -83,3 +92,4 @@ $("#option1").click(function(){
            }
          })
   });
+
